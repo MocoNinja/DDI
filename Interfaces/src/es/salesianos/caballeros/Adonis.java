05c5b24d0fbfb1c66bc899c263a4323eb175ff6a
MocoNinja <javiergonzaleztello@gmail.com>
2017-10-18 08:17:17 +0200

    Formateado to mai laikin
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- Interfaces/src/es/salesianos/caballeros/Adonis.java
+++ Interfaces/src/es/salesianos/caballeros/Adonis.java
@@ -3,25 +3,27 @@ package es.salesianos.caballeros;
 import es.salesianos.damas.Mujeres;
 import es.salesianos.damas.Princesas;
 
-public class Adonis extends Machistas {
-
+public class Adonis extends Machistas
+{
 	@Override
-	public boolean isDandoMal() {
+	public boolean isDandoMal()
+	{
 		return false;
 	}
 
 	@Override
-	public void cuidan(Mujeres mujeres) {
+	public void cuidan(Mujeres mujeres)
+	{
 		if (mujeres instanceof Princesas) {
 			System.out.println("la cuido");
 		} else {
 			super.cuidan(mujeres);
 		}
-
 	}
 
 	@Override
-	public String seDefine() {
+	public String seDefine()
+	{
 		return getClass().getName();
 	}
 
